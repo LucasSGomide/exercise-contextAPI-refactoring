d384571476046eee85750c6647a371b16b511232
Lucas Santiago Gomide <lucas.sgomide92@gmail.com>
2020-11-12 18:37:55 -0300

    Refactoring redux to contextAPI

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
 import React from 'react';
 import './App.css';
 import Cars from './Cars';
+import MyContext from './contextAPI/MyContext';
 
-function App() {
-  return (
-    <Cars />
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      red: false,
+      blue: false,
+      yellow: false,
+    }
+    
+    this.moveCar = this.moveCar.bind(this);
+  }
+
+  moveCar(car, bool) {
+    this.setState({
+      [car]:bool
+    });
+  }
+
+  render() {
+    const { red, blue , yellow } = this.state;
+
+    const cars = {
+      red,
+      blue,
+      yellow,
+      moveCar: this.moveCar,
+    }
+    return (
+      <MyContext.Provider value={cars}>
+        <Cars />
+      </MyContext.Provider>
+    )
+  }
 }
 
 export default App;
